e51cec66cc26a8f4135a75756081b7d8d08347f3
quelemine <quelemineisaacl1996@gmail.com>
2024-06-19 16:34:33 +0000

    Changed Display

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.WriteLine("Hello Prep3 World!");
+         string playAgain = "yes";
+      
+      while(playAgain.ToLower() == "yes")
+      {
+      Random randomGenerator = new Random();
+      int magicNumber = randomGenerator.Next(1, 51);
+      
+      int guess = -1;
+      int guessCount = 0;
+    //   string playAgain;
+      
+      while(guess != magicNumber)
+      {
+          Console.Write("What is your guess? ");
+          guess = int.Parse(Console.ReadLine());
+          
+          // Guess Counter
+          guessCount++;
+          
+          // Conditional statement 
+          if(magicNumber > guess)
+          {
+              Console.WriteLine("Higher");
+          }
+          else if(magicNumber < guess)
+          {
+              Console.WriteLine("Lower");
+          }
+          else
+          {
+              Console.WriteLine("\n********************************");
+              Console.WriteLine($"Guess is correct! {magicNumber}");
+              Console.WriteLine($"You guess {guessCount} times!");
+              Console.Write("********************************");
+          }
+      }
+      
+      Console.Write("\n\nDo you want to play again? yes/no: ");
+      playAgain = Console.ReadLine();
+      
+      }
+          Console.WriteLine("\nThanks for playing! ");
     }
 }
