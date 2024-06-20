7cee44a1050dae949497e1fcd5632c8ef778c23f
quelemine <quelemineisaacl1996@gmail.com>
2024-06-20 11:16:10 +0000

    Change the display

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // Job instances
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        // Job instances 2
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Google";
+        job2._startYear = 2018;
+        job2._endYear = 2024;
+
+        // resume instnce
+        Resume myResume = new Resume();
+        myResume._name = "Isaac L. Quelemine";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        
+        // Display resume Information
+        myResume.Display();
+        // job1.Display();
+        // job2.Display();
     }
 }
