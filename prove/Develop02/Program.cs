dd0701dda5f7e86b12933b89b9cd98cf34b785df
quelemine <quelemineisaacl1996@gmail.com>
2024-06-23 05:54:43 +0000

    Changed the Display

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -1,9 +1,46 @@
 using System;
-
-class Program
-{
-    static void Main(string[] args)
+using System.Collections.Generic;
+using System.IO;
+ class Program
     {
-        Console.WriteLine("Hello Develop02 World!");
+        static void Main(string[] args)
+        {
+            Journal journal = new Journal();
+            while (true)
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("1. Write a new entry");
+                Console.WriteLine("2. Display the journal");
+                Console.WriteLine("3. Save the journal to a file");
+                Console.WriteLine("4. Load the journal from a file");
+                Console.WriteLine("5. Exit");
+                Console.Write("Choose an option: ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        journal.WriteNewEntry();
+                        break;
+                    case "2":
+                        journal.DisplayJournal();
+                        break;
+                    case "3":
+                        Console.Write("Enter filename to save: ");
+                        string saveFilename = Console.ReadLine();
+                        journal.SaveJournalToFile(saveFilename);
+                        break;
+                    case "4":
+                        Console.Write("Enter filename to load: ");
+                        string loadFilename = Console.ReadLine();
+                        journal.LoadJournalFromFile(loadFilename);
+                        break;
+                    case "5":
+                        return;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
     }
-}
